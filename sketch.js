a7994ad53e95acce00d2500a44b037af494a25d2
Jnguyen7577 <jnguyen7577@yahoo.com>
2020-10-15 01:01:11 -0700

    Save Canvas

--- sketch.js
+++ sketch.js
@@ -3,14 +3,26 @@ function setup() {
   background(220);
 
   strokeWeight(7);
+
 }
 
 function draw() {
 
 if (mouseIsPressed){
 	stroke(map(mouseX, 0, 600, 0, 255, true))
-	line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-	line(mouseX, mouseY, pmouseX, pmouseY);
+	//line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+	//line(mouseX, mouseY, pmouseX, pmouseY);
 }
   
+}
+
+function keyTyped(){
+
+	if (key === 's'){
+		//save image
+		saveCanvas('fileName', 'png');
+
+	}
+
+	return false;
 }
